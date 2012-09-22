97b65f2dc10ba3fcbd184112cd87f252fe18ebbb
Roberto Vasquez Angel <robotex@robotex.de>
2012-09-22 12:51:13 +0200

    Added locale files.

--- lib/generators/ecm/sliders/locales/locales_generator.rb
+++ lib/generators/ecm/sliders/locales/locales_generator.rb
@@ -5,12 +5,19 @@ module Ecm
         desc "Copies the locale files to your application"
 
         source_root File.expand_path('../../../../../../config/locales', __FILE__)
-        
+
         def generate_locales
           copy_file "ecm.sliders.en.yml", "config/locales/ecm.sliders.en.yml"
           copy_file "ecm.sliders.de.yml", "config/locales/ecm.sliders.de.yml"
-        end   
+
+          copy_file "ecm.sliders.item.en.yml", "config/locales/ecm.sliders.item.en.yml"
+          copy_file "ecm.sliders.item.de.yml", "config/locales/ecm.sliders.item.de.yml"
+
+          copy_file "ecm.sliders.slider.en.yml", "config/locales/ecm.sliders.slider.en.yml"
+          copy_file "ecm.sliders.slider.de.yml", "config/locales/ecm.sliders.slider.de.yml"
+        end
       end
     end
   end
-end        
+end
+
